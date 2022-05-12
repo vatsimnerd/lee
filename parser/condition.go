bf951ef8b06a550a6516e181b7dc303fbba7629a
Vincent Elphick <elphickvincent@gmail.com>
2022-05-12 17:00:14 +0100

    Value type methods

--- parser/condition.go
+++ parser/condition.go
@@ -34,3 +34,41 @@ type (
 func (c Condition[T]) String() string {
 	return fmt.Sprintf("C{%s %s %s}", c.Identifier.Name, c.Operator.Token.Literal, c.Value.Token.Literal)
 }
+
+func (v Value) IsString() bool {
+	return v.String != nil
+}
+
+func (v Value) IsFloat() bool {
+	return v.Number != nil
+}
+
+func (v Value) GetStringValue() (string, error) {
+	if !v.IsString() {
+		return "", fmt.Errorf("token %s has no string value", v.Token.String())
+	}
+	return *v.String, nil
+}
+
+func (v Value) GetFloatValue() (float64, error) {
+	if !v.IsFloat() {
+		return 0, fmt.Errorf("token %s has no string value", v.Token.String())
+	}
+	return *v.Number, nil
+}
+
+func (v Value) MustGetStringValue() string {
+	str, err := v.GetStringValue()
+	if err != nil {
+		panic(err)
+	}
+	return str
+}
+
+func (v Value) MustGetFloatValue() float64 {
+	f, err := v.GetFloatValue()
+	if err != nil {
+		panic(err)
+	}
+	return f
+}
